62a4eb6cb5a86351d027c6a638895da998fa05a6
DanteMustCode <52395211+DanteMustCode@users.noreply.github.com>
2024-03-25 22:54:55 +0800

    remove controller.UpgradePolicyReconciler from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -162,13 +162,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "UpgradeTask")
 		os.Exit(1)
 	}
-	if err = (&controller.UpgradePolicyReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "UpgradePolicy")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
