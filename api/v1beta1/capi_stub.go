6045176348ce34559e28b7ffab135daedac15702
DanteMustCode <52395211+DanteMustCode@users.noreply.github.com>
2024-03-26 10:48:11 +0800

    update capi_stub.go according to CAEP

--- api/v1beta1/capi_stub.go
+++ api/v1beta1/capi_stub.go
@@ -2,41 +2,57 @@ package v1beta1
 
 import (
 	corev1 "k8s.io/api/core/v1"
-	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
-	"sigs.k8s.io/cluster-api/util/collections"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type MachineSpec struct {
-	Version                         string                  `json:"version"`
-	InfrastructureTemplateReference *corev1.ObjectReference `json:"infrastructureTemplateRef"`
+	Machine         *clusterv1.Machine
+	BootstrapConfig *unstructured.Unstructured
+	InfraMachine    *unstructured.Unstructured
 }
 
-// ControlPlaneExternalStrategy request external entity to make upgrade for controlplane
-func ControlPlaneExternalStrategy(*ControlPlaneExternalStrategyRequest, *ExternalStrategyResponse) {}
-
-// ControlPlaneExternalStrategy request external entity to make upgrade for machinedeployment
-func MachineDeploymentExternalStrategy(*MachineDeploymentExternalStrategyRequest, *ExternalStrategyResponse) {
+func ControlPlaneExternalUpgrade(*ControlPlaneExternalUpgradeRequest, *ControlPlaneExternalUpgradeResponse) {
 }
 
-// ExternalStrategyRequest is the input to an external upgrade
-// strategy implementer.
-type ControlPlaneExternalStrategyRequest struct {
-	Cluster                *clusterv1.Cluster
-	ControlPlane           *corev1.ObjectReference
-	MachinesRequireUpgrade collections.Machines
+func MachinesExternalUpgrade(*MachinesExternalUpgradeRequest, *MachinesExternalUpgradeResponse) {
+}
+
+// ControlPlaneExternalUpgradeRequest is the input to an external upgrade
+// strategy implementer for a CP.
+type ControlPlaneExternalUpgradeRequest struct {
+	metav1.TypeMeta `json:",inline"`
+	// CommonRequest contains fields common to all request types.
+	CommonRequest          `json:",inline"`
+	Cluster                corev1.ObjectReference
+	ControlPlane           corev1.ObjectReference
+	MachinesRequireUpgrade []corev1.ObjectReference
 	NewMachine             *MachineSpec
 }
 
-type MachineDeploymentExternalStrategyRequest struct {
-	Cluster                *clusterv1.Cluster
-	MachineDeployment      *corev1.ObjectReference
-	MachinesRequireUpgrade collections.Machines
-	NewMachine             *MachineSpec
-}
-
-// ExternalStrategyResponse is the response from an external
+// ControlPlaneExternalUpgradeResponse is the response from an external
 // upgrade strategy implementer.
-type ExternalStrategyResponse struct {
-	Accepted bool
-	Reason   string
+type ControlPlaneExternalUpgradeResponse struct {
+	metav1.TypeMeta `json:",inline"`
+	// CommonRetryResponse contains Status, Message and RetryAfterSeconds fields.
+	CommonRetryResponse `json:",inline"`
+}
+
+// MachinesExternalUpgradeRequest is the input to an external upgrade
+// strategy implementer for a groups of machines.
+type MachinesExternalUpgradeRequest struct {
+	metav1.TypeMeta `json:",inline"`
+	// CommonRequest contains fields common to all request types.
+	CommonRequest          `json:",inline"`
+	Cluster                corev1.ObjectReference
+	Owner                  corev1.ObjectReference
+	MachinesRequireUpgrade []corev1.ObjectReference
+	NewMachine             MachineSpec
+}
+
+// MachinesExternalUpgradeResponse is the response from an external
+// upgrade strategy implementer.
+type MachinesExternalUpgradeResponse struct {
+	metav1.TypeMeta `json:",inline"`
+	// CommonRetryResponse contains Status, Message and RetryAfterSeconds fields.
+	CommonRetryResponse `json:",inline"`
 }
