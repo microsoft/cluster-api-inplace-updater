3e8b077da604b1c8030d05449925f30c1e71f51c
DanteMustCode <52395211+DanteMustCode@users.noreply.github.com>
2024-03-26 21:42:37 +0800

    update cmd/main.go
    added upgradev1beta1 in init()
    removed infrav1 in init()
    filled setupReconcilers
    simplified setupLifecycleHookHandlers
    removed setupTopologyMutationHookHandlers
    removed (empty) setupIndexes
    removed (empty) setupWebhooks
    var metricsAddr + secureMetrics ~ diagnosticsOpts
    var probeAddr ~ healthAddr
    var enableHTTP2 ~ TBD

--- cmd/main.go
+++ cmd/main.go
@@ -19,50 +19,80 @@ package main
 import (
 	"flag"
 	"os"
-
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"k8s.io/klog/v2"
+	goruntime "runtime"
+	"time"
 
 	"github.com/spf13/pflag"
+	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/logs"
+	logsv1 "k8s.io/component-base/logs/api/v1"
+	_ "k8s.io/component-base/logs/json/register"
+	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	bootstrapv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1"
+	"sigs.k8s.io/cluster-api/controllers/remote"
+	controlplanev1 "sigs.k8s.io/cluster-api/controlplane/kubeadm/api/v1beta1"
 	runtimecatalog "sigs.k8s.io/cluster-api/exp/runtime/catalog"
 	runtimehooksv1 "sigs.k8s.io/cluster-api/exp/runtime/hooks/api/v1alpha1"
 	"sigs.k8s.io/cluster-api/exp/runtime/server"
 	"sigs.k8s.io/cluster-api/util/flags"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/cluster-api/version"
 
 	upgradev1beta1 "github.com/microsoft/cluster-api-inplace-upgrader/api/v1beta1"
 	"github.com/microsoft/cluster-api-inplace-upgrader/internal/controller"
-	"github.com/microsoft/cluster-api-inplace-upgrader/internal/handlers"
+	"github.com/microsoft/cluster-api-inplace-upgrader/internal/handlers/lifecycle"
 	//+kubebuilder:scaffold:imports
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	// scheme is a Kubernetes runtime scheme containing all the information about API types.
+	// NOTE: it is not mandatory to use scheme in custom RuntimeExtension, but working with typed API objects makes code
+	// easier to read and less error-prone than using unstructured or working with raw json/yaml.
+	scheme = runtime.NewScheme()
+	// Creates a logger to be used during the main func using controller runtime utilities
+	// NOTE: it is not mandatory to use controller runtime utilities in custom RuntimeExtension, but it is recommended
+	// because it makes log from those components similar to log from controllers.
+	setupLog       = ctrl.Log.WithName("setup")
+	controllerName = "cluster-api-inplace-upgrader-manager"
 
 	// catalog contains all information about RuntimeHooks.
-	catalog              = runtimecatalog.New()
-	metricsAddr          string
-	enableLeaderElection bool
-	probeAddr            string
-	secureMetrics        bool
-	enableHTTP2          bool
-	webhookPort          int
-	webhookCertDir       string
-	tlsOptions           = flags.TLSOptions{}
+	catalog = runtimecatalog.New()
+
+	// flags.
+	enableLeaderElection        bool
+	leaderElectionLeaseDuration time.Duration
+	leaderElectionRenewDeadline time.Duration
+	leaderElectionRetryPeriod   time.Duration
+	profilerAddress             string
+	enableContentionProfiling   bool
+	syncPeriod                  time.Duration
+	restConfigQPS               float32
+	restConfigBurst             int
+	webhookPort                 int
+	webhookCertDir              string
+	healthAddr                  string
+	tlsOptions                  = flags.TLSOptions{}
+	diagnosticsOptions          = flags.DiagnosticsOptions{}
+	logOptions                  = logs.NewOptions()
 )
 
 func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	_ = clientgoscheme.AddToScheme(scheme)
+	_ = apiextensionsv1.AddToScheme(scheme)
+
+	_ = clusterv1.AddToScheme(scheme)
+	_ = bootstrapv1.AddToScheme(scheme)
+	_ = controlplanev1.AddToScheme(scheme)
 
 	utilruntime.Must(upgradev1beta1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
@@ -73,35 +103,89 @@ func init() {
 
 // InitFlags initializes the flags.
 func InitFlags(fs *pflag.FlagSet) {
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	flag.BoolVar(&secureMetrics, "metrics-secure", false,
-		"If set the metrics endpoint is served securely")
-	flag.BoolVar(&enableHTTP2, "enable-http2", false,
-		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
+	// Initialize logs flags using Kubernetes component-base machinery.
+	// NOTE: it is not mandatory to use Kubernetes component-base machinery in custom RuntimeExtension, but it is
+	// recommended because it helps in ensuring consistency across different components in the cluster.
+	logsv1.AddFlags(logOptions, fs)
 
-	flag.IntVar(&webhookPort, "webhook-port", 9443,
+	fs.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+
+	fs.DurationVar(&leaderElectionLeaseDuration, "leader-elect-lease-duration", 15*time.Second,
+		"Interval at which non-leader candidates will wait to force acquire leadership (duration string)")
+
+	fs.DurationVar(&leaderElectionRenewDeadline, "leader-elect-renew-deadline", 10*time.Second,
+		"Duration that the leading controller manager will retry refreshing leadership before giving up (duration string)")
+
+	fs.DurationVar(&leaderElectionRetryPeriod, "leader-elect-retry-period", 2*time.Second,
+		"Duration the LeaderElector clients should wait between tries of actions (duration string)")
+
+	fs.StringVar(&profilerAddress, "profiler-address", "",
+		"Bind address to expose the pprof profiler (e.g. localhost:6060)")
+
+	fs.BoolVar(&enableContentionProfiling, "contention-profiling", false,
+		"Enable block profiling")
+
+	fs.DurationVar(&syncPeriod, "sync-period", 10*time.Minute,
+		"The minimum interval at which watched resources are reconciled (e.g. 15m)")
+
+	fs.Float32Var(&restConfigQPS, "kube-api-qps", 20,
+		"Maximum queries per second from the controller client to the Kubernetes API server. Defaults to 20")
+
+	fs.IntVar(&restConfigBurst, "kube-api-burst", 30,
+		"Maximum number of queries that should be allowed in one burst from the controller client to the Kubernetes API server. Default 30")
+
+	fs.IntVar(&webhookPort, "webhook-port", 9443,
 		"Webhook Server port")
-	flag.StringVar(&webhookCertDir, "webhook-cert-dir", "/tmp/k8s-webhook-server/serving-certs/",
+
+	fs.StringVar(&webhookCertDir, "webhook-cert-dir", "/tmp/k8s-webhook-server/serving-certs/",
 		"Webhook cert dir, only used when webhook-port is specified.")
+
+	fs.StringVar(&healthAddr, "health-addr", ":9440",
+		"The address the health endpoint binds to.")
+
+	flags.AddDiagnosticsOptions(fs, &diagnosticsOptions)
 	flags.AddTLSOptions(fs, &tlsOptions)
+
+	// NOTE: it is not mandatory to use the same flag names in all RuntimeExtension, but it is recommended when
+	// addressing common concerns like profiler-address, webhook-port, webhook-cert-dir etc. because it helps in ensuring
+	// consistency across different components in the cluster.
 }
 
+// Add RBAC for the authorized diagnostics endpoint.
+// +kubebuilder:rbac:groups=authentication.k8s.io,resources=tokenreviews,verbs=create
+// +kubebuilder:rbac:groups=authorization.k8s.io,resources=subjectaccessreviews,verbs=create
+
 func main() {
+	// Initialize and parse command line flags.
 	InitFlags(pflag.CommandLine)
 	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	// Set log level 2 as default.
 	if err := pflag.CommandLine.Set("v", "2"); err != nil {
-		setupLog.Error(err, "failed to set log level: %v")
+		setupLog.Error(err, "failed to set default log level")
 		os.Exit(1)
 	}
 	pflag.Parse()
 
+	// Validates logs flags using Kubernetes component-base machinery and apply them
+	// so klog will automatically use the right logger.
+	// NOTE: klog is the log of choice of component-base machinery.
+	if err := logsv1.ValidateAndApply(logOptions, nil); err != nil {
+		setupLog.Error(err, "unable to start extension")
+		os.Exit(1)
+	}
+
+	// Add the klog logger in the context.
+	// NOTE: it is not mandatory to use contextual logging in custom RuntimeExtension, but it is recommended
+	// because it allows to use a log stored in the context across the entire chain of calls (without
+	// requiring an addition log parameter in all the functions).
 	ctrl.SetLogger(klog.Background())
+
+	restConfig := ctrl.GetConfigOrDie()
+	restConfig.QPS = restConfigQPS
+	restConfig.Burst = restConfigBurst
+	restConfig.UserAgent = remote.DefaultClusterAPIUserAgent(controllerName)
 
 	tlsOptionOverrides, err := flags.GetTLSOptionOverrideFuncs(tlsOptions)
 	if err != nil {
@@ -109,36 +193,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	webhookServer := webhook.NewServer(webhook.Options{
-		TLSOpts: tlsOptionOverrides,
-	})
+	diagnosticsOpts := flags.GetDiagnosticsOptions(diagnosticsOptions)
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme: scheme,
-		Metrics: metricsserver.Options{
-			BindAddress:   metricsAddr,
-			SecureServing: secureMetrics,
-			TLSOpts:       tlsOptionOverrides,
-		},
-		WebhookServer:          webhookServer,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "5bb3d2b0.github.com/mogliang/",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
-	})
-	if err != nil {
-		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
+	if enableContentionProfiling {
+		goruntime.SetBlockProfileRate(1)
 	}
 
 	// Create an HTTP server for serving Runtime Extensions.
@@ -153,28 +211,50 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctrlOptions := ctrl.Options{
+		Scheme:                     scheme,
+		LeaderElection:             enableLeaderElection,
+		LeaderElectionID:           "controller-leader-election-inplace-upgrader-extension",
+		LeaseDuration:              &leaderElectionLeaseDuration,
+		RenewDeadline:              &leaderElectionRenewDeadline,
+		RetryPeriod:                &leaderElectionRetryPeriod,
+		LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
+		HealthProbeBindAddress:     healthAddr,
+		PprofBindAddress:           profilerAddress,
+		Metrics:                    diagnosticsOpts,
+		Cache: cache.Options{
+			SyncPeriod: &syncPeriod,
+		},
+		Client: client.Options{
+			Cache: &client.CacheOptions{
+				DisableFor: []client.Object{
+					&corev1.ConfigMap{},
+					&corev1.Secret{},
+				},
+			},
+		},
+		WebhookServer: runtimeExtensionWebhookServer,
+	}
+
+	// Start the manager
+	mgr, err := ctrl.NewManager(restConfig, ctrlOptions)
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	// Set up a context listening for SIGINT.
+	ctx := ctrl.SetupSignalHandler()
+
+	// Setup Runtime Extensions.
 	setupLifecycleHookHandlers(mgr, runtimeExtensionWebhookServer)
 
-	if err = (&controller.UpgradeTaskReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "UpgradeTask")
-		os.Exit(1)
-	}
-	//+kubebuilder:scaffold:builder
+	// Setup checks and reconcilers.
+	setupChecks(mgr)
+	setupReconcilers(mgr)
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
-		os.Exit(1)
-	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
-		os.Exit(1)
-	}
-
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	setupLog.Info("Starting manager", "version", version.Get().String())
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
@@ -182,32 +262,58 @@ func main() {
 
 // setupLifecycleHookHandlers sets up Lifecycle Hooks.
 func setupLifecycleHookHandlers(mgr ctrl.Manager, runtimeExtensionWebhookServer *server.Server) {
-	lifecycleExtensionHandlers := handlers.NewExtensionHandlers(mgr.GetClient())
+	// Create the ExtensionHandlers for the lifecycle hooks
+	// NOTE: it is not mandatory to group all the ExtensionHandlers using a struct, what is important
+	// is to have HandlerFunc with the signature defined in sigs.k8s.io/cluster-api/exp/runtime/hooks/api/v1alpha1.
+	lifecycleExtensionHandlers := lifecycle.NewExtensionHandlers(mgr.GetClient())
 
 	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
-		Hook:        runtimehooksv1.BeforeClusterCreate,
-		Name:        "before-cluster-create",
-		HandlerFunc: lifecycleExtensionHandlers.DoBeforeClusterCreate,
+		Hook:        runtimehooksv1.BeforeClusterUpgrade,
+		Name:        "before-cluster-upgrade",
+		HandlerFunc: lifecycleExtensionHandlers.DoBeforeClusterUpgrade,
 	}); err != nil {
 		setupLog.Error(err, "error adding handler")
 		os.Exit(1)
 	}
 
 	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
-		Hook:        upgradev1beta1.ControlPlaneExternalStrategy,
-		Name:        "call-controlplane-external-upgrade",
-		HandlerFunc: lifecycleExtensionHandlers.HandleControlPlaneExternalStrategy,
+		Hook:        runtimehooksv1.AfterControlPlaneUpgrade,
+		Name:        "after-control-plane-upgrade",
+		HandlerFunc: lifecycleExtensionHandlers.DoAfterControlPlaneUpgrade,
 	}); err != nil {
 		setupLog.Error(err, "error adding handler")
 		os.Exit(1)
 	}
 
 	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
-		Hook:        upgradev1beta1.MachineDeploymentExternalStrategy,
-		Name:        "call-machinedeployment-external-upgrade",
-		HandlerFunc: lifecycleExtensionHandlers.HandleMachineDeploymentExternalStrategy,
+		Hook:        runtimehooksv1.AfterClusterUpgrade,
+		Name:        "after-cluster-upgrade",
+		HandlerFunc: lifecycleExtensionHandlers.DoAfterClusterUpgrade,
 	}); err != nil {
 		setupLog.Error(err, "error adding handler")
 		os.Exit(1)
 	}
+}
+
+func setupChecks(mgr ctrl.Manager) {
+	if err := mgr.AddReadyzCheck("webhook", mgr.GetWebhookServer().StartedChecker()); err != nil {
+		setupLog.Error(err, "unable to create ready check")
+		os.Exit(1)
+	}
+
+	if err := mgr.AddHealthzCheck("webhook", mgr.GetWebhookServer().StartedChecker()); err != nil {
+		setupLog.Error(err, "unable to create health check")
+		os.Exit(1)
+	}
+}
+
+func setupReconcilers(mgr ctrl.Manager) {
+	if err := (&controller.UpgradeTaskReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "UpgradeTask")
+		os.Exit(1)
+	}
+	//+kubebuilder:scaffold:builder
 }
